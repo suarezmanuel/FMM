a371160651a08772839a12fe3bfda30bb7df304e
suarezmanuel <manuelsuarez244@gmail.com>
2024-03-04 18:00:02 +0200

    SIMD vectorization

--- fmm.c
+++ fmm.c
@@ -1,48 +1,55 @@
 #include "fmm.h"
+//#include <immintrin.h>
 
-// Slow fmm :)
+// we define a vector of 8 ints, using SIMD
+typedef int vec __attribute__ (( vector_size(32) ));
+
+// a helper function that allocates n vectors and initializes them with zeros
+vec* alloc(int n) {
+    vec* ptr = (vec*) aligned_alloc(32, 32 * n);
+    memset(ptr, 0, 32 * n);
+    return ptr;
+}
+
 void fmm(int n, int* m1, int* m2, int* result) {
 
-    int *_m2 = (int*) malloc (n*n*sizeof(int));
+    int n2 = n/8;
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            result[i*n+j] = 0;
-        }
-    }
+    // create two vectors
+    vec *a = alloc(n*n2);
+    vec *b = alloc(n*n2);
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            _m2[i*n+j] = m2[j*n+i];
-        }
-    }
-//
-//    int *a = (int*) malloc (8*sizeof(int));
-//    int *b = (int*) malloc (8*sizeof(int));
-
-//    for (int i = 0; i < n; i++) {
-//        for (int j = 0; j < n; j++) {
-//            for (int k = 0; k < n; k++) {
-//                result[i * n + j] += m1[i * n + k] * _m2[j * n + k];
-//            }
+//    // init result to 0
+//    for (int i = 0; i < n; ++i) {
+//        for (int j = 0; j < n; ++j) {
+//            result[i*n+j] = 0;
 //        }
 //    }
 
-    int t = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < n; k+=8) {
-                t += m1[i * n + k] * _m2[j * n + k];
-                t += m1[i * n + k+1] * _m2[j * n + k+1];
-                t += m1[i * n + k+2] * _m2[j * n + k+2];
-                t += m1[i * n + k+3] * _m2[j * n + k+3];
-                t += m1[i * n + k+4] * _m2[j * n + k+4];
-                t += m1[i * n + k+5] * _m2[j * n + k+5];
-                t += m1[i * n + k+6] * _m2[j * n + k+6];
-                t += m1[i * n + k+7] * _m2[j * n + k+7];
+    // first matrix into a
+    // transpose second matrix into b
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            // a and b are arrays of vectors of len 8
+            a[i*n + j][j % 8] = m1[i*n + j];
+            b[i*n + j][j % 8] = m2[j*n + i];
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            vec st = {0,0,0,0,0,0,0,0};
+
+            for (int k = 0; k < n2; ++k) {
+                // we are summing and multiplying vectors of len 8
+                st += a[i*n2+k] * b[j*n2+k];
             }
-            result[i * n + j] = t;
-            t=0;
+
+            for (int k = 0; k < 8; ++k) {
+                // each slot in st holds the cross-product
+                // of two n/8 long vectors
+                result[i*n+j] += st[k];
+            }
         }
     }
 }
